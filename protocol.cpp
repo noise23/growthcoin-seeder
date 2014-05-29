bdc123e155e202cd23eddbc2bc8d0dbe058c8fe9
noise23 <noise23@users.noreply.github.com>
2014-05-29 22:36:02 +0300

    pchMessageStart[4]

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0xa5, 0xef, 0xdb, 0xfd };
 
 CMessageHeader::CMessageHeader()
 {
